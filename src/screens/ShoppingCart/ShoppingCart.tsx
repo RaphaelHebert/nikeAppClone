adc73965c6d546ff21ea084c1d4ad7b377253aaa
raphael-corize <raphael.hebert@corize.co.jp>
2023-06-03 21:51:27 +0200

    added checkout button

--- src/screens/ShoppingCart/ShoppingCart.tsx
+++ src/screens/ShoppingCart/ShoppingCart.tsx
@@ -1,12 +1,60 @@
-import { View, FlatList } from 'react-native';
+import {
+  View,
+  FlatList,
+  Text,
+  useWindowDimensions,
+  Pressable,
+} from 'react-native';
 import React from 'react';
 
 import { CardListItem } from '../../components';
 import cart from '../../assets/data/cart';
 import { ICartItem } from '../../types/types';
+import styles from './styles';
+import productDetailScreenCard from '../ProductDetailScreen/styles';
+
+const { footerComponentContainer, row, text, textBold, checkoutButton } =
+  styles;
+const { buttonTextStyle, buttonStyle } = productDetailScreenCard;
+
+const FooterComponent = () => {
+  const { width } = useWindowDimensions();
+  return (
+    <View style={[footerComponentContainer, { width }]}>
+      <View style={row}>
+        <View>
+          <Text style={text}>SubTotal</Text>
+        </View>
+        <View>
+          <Text style={text}>420 US$</Text>
+        </View>
+      </View>
+      <View style={row}>
+        <View>
+          <Text style={text}>Delivery</Text>
+        </View>
+        <View>
+          <Text style={text}>20 US$</Text>
+        </View>
+      </View>
+      <View style={row}>
+        <View>
+          <Text style={[text, textBold]}>Total</Text>
+        </View>
+        <View>
+          <Text style={[text, textBold]}>440 US$</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const ShoppingCart = () => {
   const cartList: ICartItem[] = cart;
+
+  function confirmBuy() {
+    console.warn('clicked confirm buy button');
+  }
 
   return (
     <View>
@@ -15,7 +63,11 @@ const ShoppingCart = () => {
         renderItem={({ item }: { item: ICartItem }) => (
           <CardListItem cartItem={item} />
         )}
+        ListFooterComponent={FooterComponent}
       />
+      <Pressable style={[buttonStyle, checkoutButton]} onPress={confirmBuy}>
+        <Text style={buttonTextStyle}>Checkout</Text>
+      </Pressable>
     </View>
   );
 };
--- src/screens/ShoppingCart/styles.ts
+++ src/screens/ShoppingCart/styles.ts
@@ -1,3 +1,25 @@
 import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  footerComponentContainer: {
+    padding: 20,
+    paddingTop: 10,
+    borderColor: 'gainsboro',
+    borderTopWidth: 1,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+    color: 'grey',
+  },
+  textBold: {
+    fontWeight: '900',
+  },
+  checkoutButton: {
+    position: 'relative',
+    marginTop: 10,
+  },
+});
