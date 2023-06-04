0c03a4eb89d6bf1ce147225cdc0676d547cce203
raphael-corize <raphael.hebert@corize.co.jp>
2023-06-04 15:56:50 +0200

    added native stack navigator

--- src/navigators/Application.tsx
+++ src/navigators/Application.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, View } from 'react-native';
+import { SafeAreaView, StatusBar, Pressable, Text } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { useTheme } from '../hooks';
 
-import { ShoppingCart } from '../screens';
+import { ShoppingCart, ProductDetailScreen, Products } from '../screens';
 import styles from './styles';
 
+const Stack = createNativeStackNavigator();
 // @refresh reset
+
 const ApplicationNavigator = () => {
   const { Layout, darkMode, NavigationTheme } = useTheme();
   const { colors } = NavigationTheme;
 
   return (
-    <SafeAreaView style={[Layout.fill, { backgroundColor: colors.card }]}>
-      <View style={styles.outerContainer}>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          contentStyle: { backgroundColor: 'white' },
+        }}
+      >
+        {/* <SafeAreaView style={[Layout.fill, { backgroundColor: colors.card }]}> */}
         {/* <Products /> */}
-        <ShoppingCart />
-      </View>
-      <StatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} />
-    </SafeAreaView>
+        <Stack.Screen name="Products" component={Products} />
+        <Stack.Screen name="ShoppingCart" component={ShoppingCart} />
+        <Stack.Screen
+          name="ProductDetailScreen"
+          component={ProductDetailScreen}
+          options={{ presentation: 'modal' }}
+        />
+        {/* <StatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} /> */}
+        {/* </SafeAreaView> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- src/screens/Products/Products.tsx
+++ src/screens/Products/Products.tsx
@@ -1,21 +1,48 @@
-import { FlatList, Image } from 'react-native';
+import { useEffect } from 'react';
+import { FlatList, Image, Pressable, Text } from 'react-native';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import products from '../../assets/data/products';
 import styles from './styles';
 
-const { productImage } = styles;
+const { productImage, productContainer, iconContainer, iconText } = styles;
 
-const Products = () => {
+const TopRightIcon = ({ navigation }) => {
+  return (
+    <Pressable
+      style={iconContainer}
+      onPress={() => navigation.navigate('ShoppingCart')}
+    >
+      <Text style={iconText}>1</Text>
+      <FontAwesome5 name={'shopping-cart'} size={24} color="gray" />
+    </Pressable>
+  );
+};
+
+const Products = ({ navigation }) => {
+  const nav = useNavigation();
+  useEffect(() => {
+    nav.setOptions({
+      headerRight: () => <TopRightIcon navigation={navigation} />,
+    });
+  }, []);
+  function checkDetails() {
+    console.warn('navigate');
+    navigation.navigate('ProductDetailScreen');
+  }
   return (
     <FlatList
       data={products}
       renderItem={({ item, index }) => (
-        <Image
-          style={productImage}
-          source={{ uri: item.image }}
-          key={`productImage_${index}`}
-        />
+        <Pressable onPress={checkDetails} style={productContainer}>
+          <Image
+            style={productImage}
+            source={{ uri: item.image }}
+            key={`productImage_${index}`}
+          />
+        </Pressable>
       )}
       numColumns={2}
     />
